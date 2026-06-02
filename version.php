965cd265f7b234aa45c7a31420e2e64df9843310
vinit <vinitmepani2712@gmail.com>
2026-06-02 18:58:38 -0400

    Fix: Update version.php to reflect the new version number and release date.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version      = 2024120100;
+$plugin->version      = 2026060100;
 $plugin->requires     = 2022041900;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->release      = '1.0.0';
